ede111ee673d7b673ed91e71684e920c8d13e17d
Banzai99 <baba83.bn@gmail.com>
2021-01-02 19:52:44 +0100

    OSPF + LDP qui fonctionne (voir telnet maintenant)

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import gns3fy
 
-
 if __name__ == '__main__':
     gns3_server = gns3fy.Gns3Connector("http://localhost:3080")
-    lab = gns3fy.Project(name = "MerciJean", connector=gns3_server)
+    lab = gns3fy.Project(name="MerciJean", connector=gns3_server)
     lab.get()
 
     liens = lab.links
@@ -16,9 +15,9 @@ if __name__ == '__main__':
     print(topologie)
 
     for link in liens:
-        #print(link)
+        # print(link)
         for link_side in link.nodes:
-            #print(link_side)
+            # print(link_side)
             for key in topologie:
                 if (key == link_side["node_id"]) and (key == link.nodes[1]["node_id"]):
                     topologie[key][link.nodes[0]["node_id"]] = link_side["label"]["text"]
@@ -27,48 +26,45 @@ if __name__ == '__main__':
 
     print(topologie)
 
-network={}
-network_inc=1
-inc=1
+    network = {}
+    network_inc = 1
+    inc = 1
 
-for key in topologie:
-    f=open("config_R"+str(inc)+".txt", "w")
+    for key in topologie:
+        f = open("config_R" + str(inc) + ".txt", "w")
 
-    f.write("""configure terminal\n
-no ip domain lookup\n
-ip arp proxy disable\n""")
+        f.write("""configure terminal\n
+        no ip domain lookup\n
+        ip arp proxy disable\n""")
 
+        for node in topologie[key]:
+            address = 0
+            exist = False
+            try:
+                a = network[node][key]
+                exist = True
+            except KeyError:
+                pass
 
-    for node in topologie[key]:
-        address=0
-        exist=False
-        try:
-            a=network[node][key]
-            exist=True
-        except:
-            pass
+            if not exist:
+                address = network_inc
+                network[key][node] = network_inc + 1
+                network_inc += 4
+            else:
+                address = network[node][key]
 
-        if not exist:
-            address=network_inc
-            network[key] = {}
-            network[key][node]=network_inc+1
-            network_inc+=4
-        else:
-            address=network[node][key]
+            f.write(f"""interface {topologie[key][node]}
+            no shutdown
+            ip address 172.30.128.{address} 255.255.255.252
+            ip ospf 4 area 0
+            mpls ip
+            exit\n""")
 
-
-        f.write(f"""interface {topologie[key][node]}
-        no shutdown
-        ip address 172.30.128.{address} 255.255.255.252
-        ip ospf 4 area 0
-        mpls ip
-        exit\n""")
-
-    f.write(f"""router ospf 4
-router-id 0.0.0.{inc}
-redistribute connected subnets
-exit
-mpls ldp discovery targeted-hello  accept
-ip cef""")
-    f.close()
-    inc += 1
+        f.write(f"""router ospf 4
+        router-id 0.0.0.{inc}
+        redistribute connected subnets
+        exit
+        mpls ldp discovery targeted-hello  accept
+        ip cef""")
+        f.close()
+        inc += 1
