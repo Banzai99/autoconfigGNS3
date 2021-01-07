b5365f836a2579eadc8f9fcf7f5593176678437d
Banzai99 <bastian.norroy@insa-lyon.fr>
2021-01-07 22:42:42 +0100

    Identification P, PE, CE TODO : adressage du funz

--- main.py
+++ main.py
@@ -3,72 +3,85 @@ import gns3fy
 
 if __name__ == '__main__':
     gns3_server = gns3fy.Gns3Connector("http://localhost:3080")
-    lab = gns3fy.Project(name = "MerciJean", connector=gns3_server)
+    lab = gns3fy.Project(name = "tpmplsvpn", connector=gns3_server)
     lab.get()
 
     liens = lab.links
     nodes = lab.nodes
     print(len(liens))
 
-    topologie = {}
+    backbone = {}
+    edges = {}
+    custEdges = {}
     for node in nodes:
-        topologie[node.node_id] = {}
-    print(topologie)
-
+        if node.name[0:2] == "PE":
+            edges[node.node_id] = {}
+        if node.name[0] == "P":
+            backbone[node.node_id] = {}
+            continue
+        if node.name.split("_")[1][0:2] == "CE":
+            custEdges[node.node_id] = {}
+    print(backbone)
+    print(edges)
+    print(custEdges)
     for link in liens:
         #print(link)
+        print(link.capture_file_name)
         for link_side in link.nodes:
-            #print(link_side)
-            for key in topologie:
-                if (key == link_side["node_id"]) and (key == link.nodes[1]["node_id"]):
-                    topologie[key][link.nodes[0]["node_id"]] = link_side["label"]["text"]
-                elif (key == link_side["node_id"]) and (key == link.nodes[0]["node_id"]):
-                    topologie[key][link.nodes[1]["node_id"]] = link_side["label"]["text"]
+            for router in backbone:
+                if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]) and (link.nodes[0]["node_id"] in backbone.keys()):
+                    backbone[router][link.nodes[0]["node_id"]] = link_side["label"]["text"]
+                elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]) and (link.nodes[1]["node_id"] in backbone.keys()):
+                    backbone[router][link.nodes[1]["node_id"]] = link_side["label"]["text"]
+            for router in edges:
+                if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]) and not (link.nodes[0]["node_id"] in backbone.keys()):
+                    edges[router][link.nodes[0]["node_id"]] = link_side["label"]["text"]
+                elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]) and not (link.nodes[1]["node_id"] in backbone.keys()):
+                    edges[router][link.nodes[1]["node_id"]] = link_side["label"]["text"]
+            for router in custEdges:
+                if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]):
+                    custEdges[router][link.nodes[0]["node_id"]] = link_side["label"]["text"]
+                elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]):
+                    custEdges[router][link.nodes[1]["node_id"]] = link_side["label"]["text"]
 
-    print(topologie)
+    network={}
+    network_inc=1
+    inc=1
+    for router in backbone:
+        print("topo key", backbone[router])
+        f=open("config_R"+str(inc)+".txt", "w")
+        network[router] = {}
+        f.write("""configure terminal\n
+        no ip domain lookup\n
+        ip arp proxy disable\n""")
+        for node in backbone[router]:
+            address = 0
+            exist = False
+            try:
+                a = network[node][router]
+                exist = True
+            except:
+                pass
+            if not exist:
+                address = network_inc
+                network[router][node] = network_inc + 1
+                print("network ",network)
+                network_inc += 4
+            else:
+                address = network[node][router]
+            f.write(f"""interface {backbone[router][node]}
+            no shutdown
+            ip address 172.30.128.{address} 255.255.255.252
+            ip ospf 4 area 0
+            mpls ip
+            exit\n""")
 
-network={}
-network_inc=1
-inc=1
-
-for key in topologie:
-    f=open("config_R"+str(inc)+".txt", "w")
-
-    f.write("""configure terminal\n
-no ip domain lookup\n
-ip arp proxy disable\n""")
-
-
-    for node in topologie[key]:
-        address=0
-        exist=False
-        try:
-            a=network[node][key]
-            exist=True
-        except:
-            pass
-
-        if not exist:
-            address=network_inc
-            network[key] = {}
-            network[key][node]=network_inc+1
-            network_inc+=4
-        else:
-            address=network[node][key]
-
-
-        f.write(f"""interface {topologie[key][node]}
-        no shutdown
-        ip address 172.30.128.{address} 255.255.255.252
-        ip ospf 4 area 0
-        mpls ip
-        exit\n""")
-
-    f.write(f"""router ospf 4
-router-id 0.0.0.{inc}
-redistribute connected subnets
-exit
-mpls ldp discovery targeted-hello  accept
-ip cef""")
-    f.close()
-    inc += 1
+        f.write(f"""router ospf 4
+        router-id 0.0.0.{inc}
+        redistribute connected subnets
+        exit
+        mpls ldp discovery targeted-hello  accept
+        ip cef
+        exit""")
+        f.close()
+        inc += 1
