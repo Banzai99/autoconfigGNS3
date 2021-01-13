3e56196d7a29c1cedf94048df244caec62f9142a
Rémi <rejisnow69@gmail.com>
2021-01-13 11:28:40 +0100

    attribution adresse IP routeurs

--- main.py
+++ main.py
@@ -30,58 +30,92 @@ if __name__ == '__main__':
         for link_side in link.nodes:
             for router in backbone:
                 if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]) and (link.nodes[0]["node_id"] in backbone.keys()):
-                    backbone[router][link.nodes[0]["node_id"]] = link_side["label"]["text"]
+                    backbone[router][link.nodes[0]["node_id"]] = (link_side["label"]["text"], "")
                 elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]) and (link.nodes[1]["node_id"] in backbone.keys()):
-                    backbone[router][link.nodes[1]["node_id"]] = link_side["label"]["text"]
+                    backbone[router][link.nodes[1]["node_id"]] = (link_side["label"]["text"], "")
             for router in edges:
                 if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]) and not (link.nodes[0]["node_id"] in backbone.keys()):
-                    edges[router][link.nodes[0]["node_id"]] = link_side["label"]["text"]
+                    edges[router][link.nodes[0]["node_id"]] = (link_side["label"]["text"], "")
                 elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]) and not (link.nodes[1]["node_id"] in backbone.keys()):
-                    edges[router][link.nodes[1]["node_id"]] = link_side["label"]["text"]
+                    edges[router][link.nodes[1]["node_id"]] = (link_side["label"]["text"], "")
             for router in custEdges:
                 if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]):
-                    custEdges[router][link.nodes[0]["node_id"]] = link_side["label"]["text"]
+                    custEdges[router][link.nodes[0]["node_id"]] = (link_side["label"]["text"], "")
                 elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]):
-                    custEdges[router][link.nodes[1]["node_id"]] = link_side["label"]["text"]
+                    custEdges[router][link.nodes[1]["node_id"]] = (link_side["label"]["text"], "")
 
     network={}
-    network_inc=1
-    inc=1
-    for router in backbone:
-        print("topo key", backbone[router])
-        f=open("config_R"+str(inc)+".txt", "w")
-        network[router] = {}
-        f.write("""configure terminal\n
-        no ip domain lookup\n
-        ip arp proxy disable\n""")
-        for node in backbone[router]:
-            address = 0
-            exist = False
-            try:
-                a = network[node][router]
-                exist = True
-            except:
-                pass
-            if not exist:
-                address = network_inc
-                network[router][node] = network_inc + 1
-                print("network ",network)
-                network_inc += 4
-            else:
-                address = network[node][router]
-            f.write(f"""interface {backbone[router][node]}
-            no shutdown
-            ip address 172.30.128.{address} 255.255.255.252
-            ip ospf 4 area 0
-            mpls ip
-            exit\n""")
+    ipRange="172.30.128."
+    networkInc=1
 
-        f.write(f"""router ospf 4
-        router-id 0.0.0.{inc}
-        redistribute connected subnets
-        exit
-        mpls ldp discovery targeted-hello  accept
-        ip cef
-        exit""")
-        f.close()
-        inc += 1
+    for router in backbone: 
+    	for node in backbone[router]:
+    		if router in network:
+    			if node in network[router]:
+    				backbone[router][node][1]=network[router][node]
+    				continue
+    		backbone[router][node][1]=ipRange+str(networkInc)
+    		network[node][router]=ipRange+str(networkInc+1)
+    		networkInc+=4
+
+    network={}
+    ipRange="10.1."
+    subnet=1
+    loopBack="172.16.1."
+    incLb=1
+
+    for router in edges:
+    	edges[router]["lb"]=loopBack+str(incLb)
+    	incLb++
+    	for node in edges[router]:
+    		edges[router][node][1]=ipRange+str(subnet)+".2"
+    		network[node][router]=ipRange+str(subnet)+".1"
+    	subnet++
+
+    for router in custEdges:
+    	for node in custEdges[router]:
+    		custEdges[router][node][1]=network[router][node]
+
+    		
+
+    # network={}
+    # network_inc=1
+    # inc=1
+    # for router in backbone:
+    #     print("topo key", backbone[router])
+    #     f=open("config_R"+str(inc)+".txt", "w")
+    #     network[router] = {}
+    #     f.write("""configure terminal\n
+    #     no ip domain lookup\n
+    #     ip arp proxy disable\n""")
+    #     for node in backbone[router]:
+    #         address = 0
+    #         exist = False
+    #         try:
+    #             a = network[node][router]
+    #             exist = True
+    #         except:
+    #             pass
+    #         if not exist:
+    #             address = network_inc
+    #             network[router][node] = network_inc + 1
+    #             print("network ",network)
+    #             network_inc += 4
+    #         else:
+    #             address = network[node][router]
+    #         f.write(f"""interface {backbone[router][node]}
+    #         no shutdown
+    #         ip address 172.30.128.{address} 255.255.255.252
+    #         ip ospf 4 area 0
+    #         mpls ip
+    #         exit\n""")
+
+    #     f.write(f"""router ospf 4
+    #     router-id 0.0.0.{inc}
+    #     redistribute connected subnets
+    #     exit
+    #     mpls ldp discovery targeted-hello  accept
+    #     ip cef
+    #     exit""")
+    #     f.close()
+    #     inc += 1
