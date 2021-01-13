6f7e8e0e3642e5b8503d6696988cbc089fbfd59c
Banzai99 <bastian.norroy@insa-lyon.fr>
2021-01-13 11:57:17 +0100

    Fixed adressage

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
 
     liens = lab.links
     nodes = lab.nodes
-    print(len(liens))
 
     backbone = {}
     edges = {}
@@ -21,62 +20,57 @@ if __name__ == '__main__':
             continue
         if node.name.split("_")[1][0:2] == "CE":
             custEdges[node.node_id] = {}
-    print(backbone)
-    print(edges)
-    print(custEdges)
     for link in liens:
-        #print(link)
-        print(link.capture_file_name)
         for link_side in link.nodes:
             for router in backbone:
                 if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]) and (link.nodes[0]["node_id"] in backbone.keys()):
-                    backbone[router][link.nodes[0]["node_id"]] = (link_side["label"]["text"], "")
+                    backbone[router][link.nodes[0]["node_id"]] = [link_side["label"]["text"], ""]
                 elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]) and (link.nodes[1]["node_id"] in backbone.keys()):
-                    backbone[router][link.nodes[1]["node_id"]] = (link_side["label"]["text"], "")
+                    backbone[router][link.nodes[1]["node_id"]] = [link_side["label"]["text"], ""]
             for router in edges:
                 if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]) and not (link.nodes[0]["node_id"] in backbone.keys()):
-                    edges[router][link.nodes[0]["node_id"]] = (link_side["label"]["text"], "")
+                    edges[router][link.nodes[0]["node_id"]] = [link_side["label"]["text"], ""]
                 elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]) and not (link.nodes[1]["node_id"] in backbone.keys()):
-                    edges[router][link.nodes[1]["node_id"]] = (link_side["label"]["text"], "")
+                    edges[router][link.nodes[1]["node_id"]] = [link_side["label"]["text"], ""]
             for router in custEdges:
                 if (router == link_side["node_id"]) and (router == link.nodes[1]["node_id"]):
-                    custEdges[router][link.nodes[0]["node_id"]] = (link_side["label"]["text"], "")
+                    custEdges[router][link.nodes[0]["node_id"]] = [link_side["label"]["text"], ""]
                 elif (router == link_side["node_id"]) and (router == link.nodes[0]["node_id"]):
-                    custEdges[router][link.nodes[1]["node_id"]] = (link_side["label"]["text"], "")
+                    custEdges[router][link.nodes[1]["node_id"]] = [link_side["label"]["text"], ""]
 
-    network={}
-    ipRange="172.30.128."
-    networkInc=1
+    network = {}
+    ipRange = "172.30.128."
+    networkInc = 1
 
     for router in backbone: 
-    	for node in backbone[router]:
-    		if router in network:
-    			if node in network[router]:
-    				backbone[router][node][1]=network[router][node]
-    				continue
-    		backbone[router][node][1]=ipRange+str(networkInc)
-    		network[node][router]=ipRange+str(networkInc+1)
-    		networkInc+=4
+        for node in backbone[router]:
+            if router in network:
+                if node in network[router]:
+                    backbone[router][node][1] = network[router][node]
+                    continue
+            backbone[router][node][1] = ipRange+str(networkInc)
+            network[node] = {}
+            network[node][router] = ipRange+str(networkInc+1)
+            networkInc += 4
 
-    network={}
-    ipRange="10.1."
-    subnet=1
-    loopBack="172.16.1."
-    incLb=1
+    network = {}
+    ipRange = "10.1."
+    subnet = 1
+    loopBack = "172.16.1."
+    incLb = 1
 
     for router in edges:
-    	edges[router]["lb"]=loopBack+str(incLb)
-    	incLb++
-    	for node in edges[router]:
-    		edges[router][node][1]=ipRange+str(subnet)+".2"
-    		network[node][router]=ipRange+str(subnet)+".1"
-    	subnet++
+        for node in edges[router]:
+            edges[router][node][1] = ipRange+str(subnet)+".2"
+            network[node] = {}
+            network[node][router] = ipRange+str(subnet)+".1"
+        edges[router]["lb"] = loopBack+str(incLb)
+        incLb += 1
+        subnet += 1
 
     for router in custEdges:
-    	for node in custEdges[router]:
-    		custEdges[router][node][1]=network[router][node]
-
-    		
+        for node in custEdges[router]:
+            custEdges[router][node][1] = network[router][node]
 
     # network={}
     # network_inc=1
